6a9178826bd8d583f581c3e0ec2d8550cba3e503
Kenneth MacCallum <kenneth@offshoredesigns.ca>
2025-01-01 11:13:49 -0800

    still starting

---
+++ src/com/bluerobotics/blueberry/transcoder/java/BlueberryReceiver.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bluerobotics.blueberry.transcoder.java;
+
+import com.starfishmedical.comms.Packet;
+import com.starfishmedical.comms.PacketReceiver;
+
+/**
+ * 
+ */
+public class BlueberryReceiver extends PacketReceiver {
+	private Packet m_packet;
+	@Override
+	public void use(byte[] bs, int bo, int bl) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
